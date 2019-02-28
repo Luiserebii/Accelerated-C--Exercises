435f0142a3b56072e5f65e9875310f8eac8ca44f
Luiserebii <luis@serebii.io>
2019-02-28 18:23:30 -0500

    Added 3_0 and 3_1

---
+++ chapter_exercises/ch3/3_1.cpp
@@ -0,0 +1,20 @@
+/*
+
+Suppose we wish to find the median of a collection of values. Assume that we have read some of the values so far, and that we have no idea how many values remain to be read. Prove that we cannot afford to discard any of the values that we have read. 
+
+Hint: One proof strategy is to assume that we can discard a value, and then find values for the unread—and therefore unknown—part of our collection that would cause the median to be the value that we discarded.
+
+
+
+////////////////////////////////////////////
+
+
+I'm not sure I understand this question... doesn't this answer itself? If we do not know how many values remain to be read, assuming that the values are sorted, we can't determine where the "middle" unless we gather them up. As the hint says, we could have discarded the median, and thus would not be able to find the true one. Is there some formal proof solution I'm missing here?
+
+
+
+
+*/
+
+
+
