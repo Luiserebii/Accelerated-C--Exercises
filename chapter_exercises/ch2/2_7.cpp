8e95da7d8387130c66eb20b1017b3c48e42d2e90
Luiserebii <luis@serebii.io>
2019-02-28 17:39:36 -0500

    Added 2_6.cpp and 2_7.cpp

---
+++ chapter_exercises/ch2/2_7.cpp
@@ -0,0 +1,15 @@
+//Write a program to count from 10 to -5
+
+#include <iostream>
+
+
+int main()
+{
+   
+   for(int i = 10; i > -6; --i){
+      std::cout << i << std::endl;
+   }
+   return 0;
+}
+
+
