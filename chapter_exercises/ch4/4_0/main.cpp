f9af480daa82867ba5db3ecf21b26a1e84c87b8b
Luiserebii <luis@serebii.io>
2019-03-03 22:59:21 -0500

    More code added to main.cpp file; the one before it is split up

--- chapter_exercises/ch4/4_0/main.cpp
+++ chapter_exercises/ch4/4_0/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 
 using std::cout;
@@ -8,7 +10,26 @@ using std::cin;
 using std::endl;
 
 using std::string;
+using std::vector;
+using std::max;
+using std::setprecision;
 
+using std::domain_error;
+struct Student_info {
+   string name;
+   double midterm, final;
+   vector<double> homework;
+};
+
+
+std::istream& read(std::istream& id, Student_info& s);
+std::istream& read_hw(std::istream& in, vector<double>& hw);
+
+double grade(const Student_info& s);
+double grade(double midterm, double final, double homework);
+
+bool compare(const Student_info& x, const Student_info& y);
+double median(vector<double> vec);
 
 int main() 
 {
@@ -33,7 +54,7 @@ int main()
       // compute and write the grade
       try {
          double final_grade = grade(students[i]);
-         streamsize prec = cout.precision(); //getting precision of istream obj
+         std::streamsize prec = cout.precision(); //getting precision of istream obj
          cout << setprecision(3) << final_grade << setprecision(prec);
          
       } catch (domain_error e) {
@@ -43,6 +64,63 @@ int main()
 
    return 0;
    
+}
+
+
+std::istream& read(std::istream& is, Student_info& s){
+   // read and store the student's name and midterm and final exam grades
+   is >> s.name >> s.midterm >> s.final;
+
+   read_hw(is, s.homework); // read and store all the student's homework grades
+   return is;
+}
+
+std::istream& read_hw(std::istream& in, vector<double>& hw){
+   if(in) {
+      // get rid of previous contents
+      hw.clear();
+
+      // read homework grades
+      double x;
+      while (in >> x) {
+         hw.push_back(x);
+      }
+
+      // clear the stream so that input will owrk for the next student
+      in.clear();
+
+   }
+   return in;
+}
+
+
+double grade(const Student_info& s){
+   return grade(s.midterm, s.final, s.homework);
+}
+
+double grade(double midterm, double final, double homework)
+{
+   return 0.2 * midterm + 0.4 * final + 0.4 * homework;
+}
+
+bool compare(const Student_info& x, const Student_info& y){
+   return x.name < y.name;
+}
+
+double median(vector<double> vec){
+   typedef vector<double>::size_type vec_sz;
+
+   vec_sz size = vec.size();
+   if (size == 0) {
+      throw domain_error("median of an empty vector");
+   }
+ 
+   sort(vec.begin(), vec.end());
+
+   vec_sz mid = size/2;
+   
+   return size % 2 == 0 ? (vec[mid] + vec[mid-1]) / 2 : vec[mid];
+
 }
 
 
