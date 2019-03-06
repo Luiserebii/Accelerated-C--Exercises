57d0622b6f1b5807459dec14e3e5b6040b875611
Luiserebii <luis@serebii.io>
2019-03-06 00:18:45 -0500

    Basic piece of first example written!

--- chapter_exercises/ch5/5_0/1.cpp
+++ chapter_exercises/ch5/5_0/1.cpp
@@ -7,6 +7,8 @@
 using std::string;
 using std::vector;
 
+using std::cout;
+using std::endl;
 
 struct Student_info {
 
@@ -26,6 +28,19 @@ double grade(double midterm, double final, vector<double> homework);
 int main() {
 
    Student_info s1 = { "Floofy", 100, 100, {100, 100, 100} }; 
+   Student_info s2 = { "Doot Doot", 0, 0, {60, 50}};
+   Student_info s3 = { "Cake", 100, 80, {0}};
+   Student_info s4 = { "Floofy-mega", 1000, 1000, {10000}};
+
+   vector<Student_info> passing = {s1, s2, s3, s4};
+   vector<Student_info> failing = extract_fails(passing);
+    
+   typedef vector<Student_info>::const_iterator student_iter;
+
+   for(student_iter iter = passing.begin(); iter < passing.end(); ++iter){
+      cout << "PASSING STUDENTS: " << (*iter).name << endl;
+
+   }
 
 }
 
