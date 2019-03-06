1529a78438c8979fbf6d8266a5741d7eeb0f91fa
Luiserebii <luis@serebii.io>
2019-03-05 22:18:12 -0500

    Starting Ch. 5 Exercises

---
+++ chapter_exercises/ch5/5_0/1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+
+using std::string;
+using std::vector;
+
+
+struct Student_info {
+
+   std::string name;
+   double midterm, final;
+   std::vector<double> homework;
+
+}
+
+
+
+
+int main() {
+
+
+
+}
