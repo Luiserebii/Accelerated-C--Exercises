34824b1fc7a23de122b31b9123151e83ba7f5eaa
Luiserebii <luis@serebii.io>
2019-03-06 00:50:25 -0500

    More breaking out

--- chapter_exercises/ch5/5_0/c.sh
+++ chapter_exercises/ch5/5_0/c.sh
@@ -1,4 +1,5 @@
 MAIN=$1
 LIB=lib
+CHAPTER_CODE=chapter_code.cpp
 
-g++ -std=c++11 $MAIN $LIB/*.cpp
+g++ -std=c++11 $MAIN $LIB/*.cpp $CHAPTER_CODE
---
+++ chapter_exercises/ch5/5_0/chapter_code.h
@@ -0,0 +1,14 @@
+#ifndef GUARD_chapter_code
+#define GUARD_chapter_code
+
+/*================================================*/
+
+#include <vector>
+#include "lib/student_lib.h"
+
+std::vector<Student_info> extract_fails(std::vector<Student_info>& students);
+
+
+/*=================================================*/
+
+#endif
