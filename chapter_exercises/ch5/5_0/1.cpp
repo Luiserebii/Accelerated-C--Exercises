80216b51f513f5b3650b48ed5bdd00eebdecf96a
Luiserebii <luis@serebii.io>
2019-03-05 22:42:30 -0500

    Added code needed to run extract_fails, created small compile script to allow for easy compilation. Looking to break out the grade stuff "dependencies" into their own sort of folder with headers, both as practice, and to have a cleaner method to test between functions without having to comment chunks of code out

--- chapter_exercises/ch5/5_0/1.cpp
+++ chapter_exercises/ch5/5_0/1.cpp
@@ -10,17 +10,62 @@ using std::vector;
 
 struct Student_info {
 
-   std::string name;
+   string name;
    double midterm, final;
-   std::vector<double> homework;
+   vector<double> homework;
 
-}
+};
 
+vector<Student_info> extract_fails(vector<Student_info>& students);
+bool fgrade(const Student_info& s);
+double grade(const Student_info& s);
+double grade(double midterm, double final, vector<double> homework);
 
 
 
 int main() {
 
-
+   Student_info s1 = { "Floofy", 100, 100, {100, 100, 100} }; 
 
 }
+
+
+vector<Student_info> extract_fails(vector<Student_info>& students){
+
+   vector<Student_info> pass, fail;
+   
+   for(vector<Student_info>::size_type i = 0; i < students.size(); ++i){
+
+      if(fgrade(students[i]))
+         fail.push_back(students[i]);
+      else
+         pass.push_back(students[i]);      
+      
+   }
+
+   students = pass;
+   return fail;
+   
+}
+
+
+
+
+
+bool fgrade(const Student_info& s){
+   return grade(s) < 60;
+}
+
+double grade(const Student_info& s){
+   return grade(s.midterm, s.final, s.homework);
+}
+
+double grade(double midterm, double final, vector<double> homework) {
+
+   int hw = 100;
+
+   return 0.2 * midterm + 0.4 * final + 0.4 * hw;
+   //we're hardcoding homework to 100 for simplicity
+
+}
+
---
+++ chapter_exercises/ch5/5_0/cmp.sh
@@ -0,0 +1,3 @@
+MAIN=$1
+
+g++ $MAIN -std=c++11
