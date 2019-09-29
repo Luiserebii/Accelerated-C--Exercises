4217944f0e5d302c10c1482f3d7284de6921c1ab
Luiserebii <luis@serebii.io>
2019-09-29 01:51:14 -0400

    This first piece is compiling

--- chapter_exercises/ch6/6_0/main.cpp
+++ chapter_exercises/ch6/6_0/main.cpp
@@ -36,12 +36,22 @@ bool not_space(char c) {
 
 vector<string> split(const string& str) {
     typedef string::const_iterator iter;
-    vector<string> strs;
+    vector<string> ret;
 
     iter i = str.begin();
     while(i != str.end()) {
+        //Look for an iterator pointing to element which is not a space
         i = find_if(i, str.end(), not_space);
-
+        //Find end of next word
+        iter j = find_if(i, str.end(), space);
+        //Copy the characters in [i, j), assuming we haven't hit the end in looking for a non-space character
+        if(i != str.end()) {
+            //Add this new string, using iterators as the range
+            ret.push_back(string(i, j));
+        }
+        //Push our loop "pointer" up
+        i = j;
     
     }
+    return ret;
 }
