c5092f60cb86ac579ce9b5b4120482a28f4d07af
Luiserebii <luis@serebii.io>
2019-09-29 21:05:12 -0400

    Starting refactoring process

---
+++ chapter_exercises/ch6/6_0/student/student.h
@@ -0,0 +1,24 @@
+#ifndef GUARD_student_6_h
+#define GUARD_student_6_h
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+//#include "./lib/median.h"
+#include "./lib/student_info.h"
+//#include "./lib/grade.h"
+
+
+bool did_all_hw(const Student_info& s);
+double grade_aux(const Student_info& s);
+double median_analysis(const std::vector<Student_info>& students);
+void write_analysis(std::ostream& out, const std::string& name, double analysis(const std::vector<Student_info>&), const std::vector<Student_info>& did, const std::vector<Student_info>& didnt);
+double average(const std::vector<double>& v);
+double average_grade(const Student_info& s);
+double average_analysis(const std::vector<Student_info>& students);
+double optimistic_median(const Student_info& s);
+bool pgrade(const Student_info& s);
+std::vector<Student_info> extract_fails(std::vector<Student_info>& students);
+
+#endif
