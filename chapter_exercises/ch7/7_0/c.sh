81f46d858c8d534295bfa80e450526eef1a97b7b
Luiserebii <luis@serebii.io>
2019-10-01 19:29:21 -0400

    Adjusted compiling script

--- chapter_exercises/ch7/7_0/c.sh
+++ chapter_exercises/ch7/7_0/c.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-if [ $1 == 2 ]
+if [ $1 == 1 ]
+then
+	g++ counting-words.cpp && ./a.out
+elif [ $1 == 2 ]
 then
 	g++ cross-reference.cpp ../../ch5/5_0/chapter_code/chapter_code.cpp && ./a.out
+else
+	g++ $1 && ./a.out
 fi
