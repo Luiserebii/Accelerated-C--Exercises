21939e62dbf71a69e8a0f17050e820ecc6ddb2ad
Luiserebii <luis@serebii.io>
2019-10-01 19:51:02 -0400

    Wrote gen_aux() function alongside comments

--- chapter_exercises/ch7/7_0/grammar.cpp
+++ chapter_exercises/ch7/7_0/grammar.cpp
@@ -18,6 +18,8 @@ typedef vector<Rule> Rule_collection;
 typedef map<string, Rule_collection> Grammar;
 
 Grammar read_grammar(istream& in);
+vector<string> gen_sentence(const Grammar& g);
+bool bracketed(const string& s);
 
 int main() {
 
@@ -52,4 +54,26 @@ vector<string> gen_sentence(const Grammar& g) {
 //Return whether string is bracked (like <this>) or not
 bool bracketed(const string& s) {
     return s.size() > 1 && s[0] == '<' && s[s.size() - 1] == '>';
+}
+
+void gen_aux(const Grammar& g, const string& word, vector<string>& ret) {
+    //If the word isn't bracketed, then we can feel free to push it as is
+    if(!bracketed(word)) {
+        ret.push_back(word);
+    } else {
+        //Grab the rule
+        Grammar::const_iterator it = g.find(word);
+        if(it == g.end()) {
+            throw logic_error("empty rule");
+        }
+        //Grab a reference to the rule collection for that specific rule 
+        //(i.e. the vector<string>)
+        const Rule_collection& c = it->second;   
+        //Randomly obtain a number
+        const Rule& r = c[nrand(c.size())];
+
+        for(Rule::const_iterator i = r.begin(); i != r.end(); ++i) {
+            gen_aux(g, *i, ret);
+        }
+    }
 }
