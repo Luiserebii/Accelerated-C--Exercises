1751b1fdd9610202b2c68e4dd1d12233c6b4b3af
Luiserebii <luis@serebii.io>
2019-10-01 19:45:27 -0400

    Continuing to add code; comments added to read_grammar()

--- chapter_exercises/ch7/7_0/grammar.cpp
+++ chapter_exercises/ch7/7_0/grammar.cpp
@@ -17,16 +17,39 @@ typedef vector<string> Rule;
 typedef vector<Rule> Rule_collection;
 typedef map<string, Rule_collection> Grammar;
 
+Grammar read_grammar(istream& in);
+
+int main() {
+
+    return 0;
+}
+
 Grammar read_grammar(istream& in) {
     Grammar ret;
     string line;
-    
+   
+    //Loop through each line 
     while(getline(in, line)) {
+        //Grab a line, in words
         vector<string> entry = split(line);
 
+        //Assuming it isn't empty,
         if(!entry.empty()) {
+            //Using the first word as the map string value, push_back a new vector created utilizing the iterators
+            //and thrown into the Rule() (and therefore, vector<string>) constructor
             ret[entry[0]].push_back(Rule(entry.begin() + 1, entry.end()));
         }
     }
     return ret;
+}
+
+vector<string> gen_sentence(const Grammar& g) {
+    vector<string> ret;
+    gen_aux(g, "<sentence>", ret);
+    return ret;
+}
+
+//Return whether string is bracked (like <this>) or not
+bool bracketed(const string& s) {
+    return s.size() > 1 && s[0] == '<' && s[s.size() - 1] == '>';
 }
