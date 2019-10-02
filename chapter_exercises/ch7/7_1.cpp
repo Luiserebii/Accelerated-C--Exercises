0f9d08ee43f838d261ca73b5f45644df87fa4326
Luiserebii <luis@serebii.io>
2019-10-01 21:41:02 -0400

    Compiling and running well, 7_1.cpp appears to be complete!

--- chapter_exercises/ch7/7_1.cpp
+++ chapter_exercises/ch7/7_1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <map>
 
 using std::cin;
@@ -8,7 +9,11 @@ using std::endl;
 using std::ostream;
 
 using std::string;
+using std::vector;
 using std::map;
+
+ostream& writeGroupedOccurences(ostream& out, const map<string, int>& m);
+void mapGroupOccurences(const map<string, int>& m, map<int, vector<string> >& groupOccurences);
 
 int main() {
     string s;
@@ -21,18 +26,20 @@ int main() {
     for(map<string, int>::const_iterator it = counters.begin(); it != counters.end(); ++it) {
         cout << it->first << "\t" << it->second << endl;
     }
+    writeGroupedOccurences(cout, counters);
+
     return 0;
 }
 
 ostream& writeGroupedOccurences(ostream& out, const map<string, int>& m) {
     out << "Ordered by occurence count: " << endl << endl;
-    const map<int, vector<string> > groupOccurences;
+    map<int, vector<string> > groupOccurences;
     mapGroupOccurences(m, groupOccurences);
     for(map<int, vector<string> >::const_iterator i = groupOccurences.begin(); i != groupOccurences.end(); ++i) {
         //Write the int
         out << i->first << ": " << endl;
         //Iterate over vector<string> and print each thingie
-        for(vector<string>::const_iterator j = i->second.begin(); j != i->second.end(); ++i) {
+        for(vector<string>::const_iterator j = i->second.begin(); j != i->second.end(); ++j) {
             out << "    " << *j << endl;
         }
         cout << endl;
@@ -40,7 +47,7 @@ ostream& writeGroupedOccurences(ostream& out, const map<string, int>& m) {
     return out;
 }
 
-void mapGroupOccurences(const map<string, int>& m, const map<int, vector<string> >& groupOccurences) {
+void mapGroupOccurences(const map<string, int>& m, map<int, vector<string> >& groupOccurences) {
     for(map<string, int>::const_iterator i = m.begin(); i != m.end(); ++i) {
         groupOccurences[i->second].push_back(i->first);
     }
