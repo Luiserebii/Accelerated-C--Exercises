a221b9f515a91bec2818de6aa81736091ce3a438
Luiserebii <luis@serebii.io>
2019-10-03 02:59:22 -0400

    Setting up proper imports/compilation script for 7_8.cpp

--- chapter_exercises/ch7/c.sh
+++ chapter_exercises/ch7/c.sh
@@ -3,6 +3,9 @@
 if [ $1 == 3 ] || [ $1 == 4 ] || [ $1 == 5 ] || [ $1 == 6 ] || [ $1 == 7 ]
 then
 	g++ 7_$1.cpp ../ch5/5_0/chapter_code/chapter_code.cpp && ./a.out
+elif [ $1 == 8 ]
+then
+	g++ 7_$1.cpp ../ch6/6_0/chapter_code/chapter_code.cpp && ./a.out
 else
 	g++ $1 && ./a.out
 fi
