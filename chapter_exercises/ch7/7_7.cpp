593e21c7582b6f8472d02d8feb28344720c5cd10
Luiserebii <luis@serebii.io>
2019-10-03 02:50:05 -0400

    Finished 7_7.cpp. Skipping 7_6.cpp for the moment since it may take a day when I'm a little more awake

---
+++ chapter_exercises/ch7/7_7.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <stdexcept>
+
+#include "../ch5/5_0/chapter_code/chapter_code.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::istream;
+
+
+using std::string;
+using std::getline;
+using std::vector;
+using std::map;
+using std::logic_error;
+
+map<string, vector<int> > xref(istream& in, vector<string> find_words(const string&) = split);
+
+int main() {
+    
+    map<string, vector<int> > ret = xref(cin);
+
+    for(map<string, vector<int> >::const_iterator i = ret.begin(); i != ret.end(); ++i) {
+        if(i->second.size() == 1) {
+            //Write the first word
+            cout << i->first << " occurs on line: " << *(i->second.begin()) << endl;
+        } else if(i->second.size() > 1) {
+            //Write the first word
+            cout << i->first << " occurs on line(s): " ;
+
+            //And next, the line numbers:
+            vector<int>::const_iterator line_it = i->second.begin();
+            //Write the first one, and...
+            cout << *line_it;
+            ++line_it;
+            //..the rest, if any
+            while(line_it != i->second.end()) {
+                cout << ", " << *line_it;
+                ++line_it;
+            }
+            cout << endl;
+        } else {
+            throw logic_error("Error: cross-referenced mapping contained word with an empty vector<int> (no line numbers found). This should be unreachable; this was found for the string: " + i->first);
+        }
+    }    
+    return 0;
+}
+
+/**
+ * Cross-referencing function, which will take input from any stream, a function to acquire a vector<string>
+ * of words from that stream (like split, as default), and return a mapping of strings to vector<ints>, 
+ * representing their locations found by line number
+ */
+map<string, vector<int> > xref(istream& in, vector<string> find_words(const string&)) {
+    string line;
+    int line_number = 0;
+    map<string, vector<int> > ret;
+
+    //Loop through each line
+    while(getline(in, line)) {
+        ++line_number;
+        //Grab words in this specific line
+        vector<string> words = find_words(line);
+        //For each word...
+        for(vector<string>::const_iterator i = words.begin(); i != words.end(); ++i) {
+            //Target the string in the map and push_back the line number
+            ret[*i].push_back(line_number);
+        }
+    }
+    return ret;
+}
+
+
--- chapter_exercises/ch7/c.sh
+++ chapter_exercises/ch7/c.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $1 == 3 ] || [ $1 == 4 ] || [ $1 == 5 ] || [ $1 == 6 ]
+if [ $1 == 3 ] || [ $1 == 4 ] || [ $1 == 5 ] || [ $1 == 6 ] || [ $1 == 7 ]
 then
 	g++ 7_$1.cpp ../ch5/5_0/chapter_code/chapter_code.cpp && ./a.out
 else
