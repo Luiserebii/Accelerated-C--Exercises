5f6f4b4aa61b4e0e9a4b3205b12d5ba479e8e960
Luiserebii <luis@serebii.io>
2019-10-09 00:12:25 -0400

    Wrote copy for 8_2.cpp

--- chapter_exercises/ch8/8_2.cpp
+++ chapter_exercises/ch8/8_2.cpp
@@ -4,7 +4,23 @@ template <class InputIterator>
 bool equal(InputIterator begin, InputIterator end, InputIterator begin2);
 
 int main() {
-
-
     return 0;
+}
+
+
+template <class InputIterator>
+bool equal(InputIterator begin, InputIterator end, InputIterator begin2) {
+
+    while(begin != end) {
+        if(*begin++ != *begin2++) return false;
+        /**
+         * Shorthand, for below:
+         *
+         * begin++
+         * begin2++
+         **/
+    }
+    //If we hit here, it must be true, since everything was equal hitherto
+    return true;
+
 }
