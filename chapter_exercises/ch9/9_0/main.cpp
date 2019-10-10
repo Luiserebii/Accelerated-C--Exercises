623272862ef859fec5ddeb326e519530c9b6af67
Luiserebii <luis@serebii.io>
2019-10-10 03:35:23 -0400

    Adding in basic class

--- chapter_exercises/ch9/9_0/main.cpp
+++ chapter_exercises/ch9/9_0/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <vector>
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::istream;
+
+using std::vector;
+
+int main() {
+
+
+}
+
+
+class Student_info {
+    
+    public: 
+        double grade() const;
+        std::istream& read(std::istream&);
+
+    private:
+        std::string name;
+        double midterm, final;
+        std::vector<double> homework;
+}
 
 istream& Student_info::read(istream& in) {
     in >> name >> midterm >> final;
@@ -10,20 +32,7 @@ istream& Student_info::read(istream& in) {
     return in;
 }
 
-/**
-struct Student_info {
-    std::string name;
-    double midterm, final;
-     std::vector<double> homework;
+double Student_info::grade() const {
+    return ::grade(midterm, final, homework);
 }
-*/
-
-/**
-struct Student_info {
-    std::string name; 
-    double midterm, final;
-    std::vec
-}
-*/
-
 
