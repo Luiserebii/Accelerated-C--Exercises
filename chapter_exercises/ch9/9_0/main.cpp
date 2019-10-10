5974d5e67ed2a410b88404e0ef53a7ce8d5b333c
Luiserebii <luis@serebii.io>
2019-10-10 04:00:48 -0400

    Sticking together imports... now to organize

--- chapter_exercises/ch9/9_0/main.cpp
+++ chapter_exercises/ch9/9_0/main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <string>
 
+#include "grade.h"
+
 using std::cin;
 using std::cout;
 using std::endl;
@@ -32,7 +34,7 @@ class Student_info {
 };
 
 bool compare(const Student_info& x, const Student_info& y);
-
+istream& read_hw(istream& in, vector<double>& hw);
 
 
 Student_info::Student_info(): midterm(0), final(0) {
@@ -44,7 +46,7 @@ Student_info::Student_info(istream& is) {
 }
 
 istream& Student_info::read(istream& in) {
-    in >> name >> midterm >> final;
+    in >> n >> midterm >> final;
     read_hw(in, homework);
     return in;
 }
@@ -56,3 +58,23 @@ double Student_info::grade() const {
 bool compare(const Student_info& x, const Student_info& y) {
     return x.name() < y.name();
 }
+
+istream& read_hw(istream& in, vector<double>& hw){
+   if(in) {
+      // get rid of previous contents
+      hw.clear();
+
+      // read homework grades
+      double x;
+      while (in >> x) {
+         hw.push_back(x);
+      }
+
+      // clear the stream so that input will work for the next student
+      //last line in the pattern, but could we also put this at the end of read...? I guess no point if modular, but... hmm.
+      in.clear();
+
+   }
+   return in;
+}
+
