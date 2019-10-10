2d50ae5ef85c32d87de0be3aa7631e0c0076add5
Luiserebii <luis@serebii.io>
2019-10-10 04:21:21 -0400

    Great, compiling!

--- chapter_exercises/ch9/9_0/main.cpp
+++ chapter_exercises/ch9/9_0/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <stdexcept>
 
 #include "student/student_info.h"
 
@@ -9,11 +11,15 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+using std::streamsize;
+using std::setprecision;
+
 using std::vector;
 using std::string;
 
 using std::max;
 using std::sort;
+using std::domain_error;
 
 int main() {
 
@@ -30,6 +36,18 @@ int main() {
     //Alphabetize the student records
     sort(students.begin(), students.end(), compare);
 
-
+    //Write names and grades
+    for(vector<Student_info>::size_type i = 0; i != students.size(); ++i) {
+        cout << students[i].name() << string(maxlen + 1 - students[i].name().size(), ' ');
+        try {
+            double final_grade = students[i].grade();
+            streamsize prec = cout.precision();
+            cout << setprecision(3) << final_grade
+                << setprecision(prec) << endl;
+        } catch(domain_error e) {
+            cout << e.what() << endl;
+        }
+    }
+    return 0;
 }
 
