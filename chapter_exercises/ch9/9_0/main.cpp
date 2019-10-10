f4f51d7b48fc5c1cab0c3f1135331dd685d44dd1
Luiserebii <luis@serebii.io>
2019-10-10 03:44:00 -0400

    Everything packed in, just need to import correct pieces and reorganize

--- chapter_exercises/ch9/9_0/main.cpp
+++ chapter_exercises/ch9/9_0/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using std::cin;
 using std::cout;
@@ -13,17 +14,33 @@ int main() {
 
 }
 
-
 class Student_info {
     
     public: 
-        double grade() const;
+        Student_info();
+        Student_info(std::istream&);
+
+        std::string name() const { return n; }
+        bool valid() const { return !homework.empty(); }
         std::istream& read(std::istream&);
+        double grade() const;
 
     private:
-        std::string name;
+        std::string n;
         double midterm, final;
         std::vector<double> homework;
+};
+
+bool compare(const Student_info& x, const Student_info& y);
+
+
+
+Student_info::Student_info(): midterm(0), final(0) {
+
+}
+
+Student_info::Student_info(istream& is) { 
+    read(is);
 }
 
 istream& Student_info::read(istream& in) {
@@ -36,3 +53,6 @@ double Student_info::grade() const {
     return ::grade(midterm, final, homework);
 }
 
+bool compare(const Student_info& x, const Student_info& y) {
+    return x.name() < y.name();
+}
