7f336c3258ddca3e304a1f1eb2b89577990365b5
Luiserebii <luis@serebii.io>
2019-10-10 04:14:10 -0400

    Fleshing out usage of Student_info class

--- chapter_exercises/ch9/9_0/main.cpp
+++ chapter_exercises/ch9/9_0/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 #include "student/student_info.h"
 
@@ -8,7 +9,26 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+using std::vector;
+using std::string;
+
+using std::max;
+using std::sort;
+
 int main() {
+
+    vector<Student_info> students;
+    Student_info record;
+    string::size_type maxlen = 0;
+
+    //Read and store data
+    while(record.read(cin)) {
+        maxlen = max(maxlen, record.name().size());
+        students.push_back(record);
+    }
+
+    //Alphabetize the student records
+    sort(students.begin(), students.end(), compare);
 
 
 }
