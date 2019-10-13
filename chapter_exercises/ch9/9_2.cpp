9362b2cbb07d212dbd6d82e2e97d6199bc1b0635
Luiserebii <luis@serebii.io>
2019-10-12 21:09:35 -0400

    Adding 9_2.cpp

---
+++ chapter_exercises/ch9/9_2.cpp
@@ -0,0 +1,8 @@
+/**
+ * 9-2. If we define the name function as a plain, nonconst member function,
+ * what other functions in our system must change and why?
+ *
+ * const member functions have no restrictions in how they are used,
+ * only in how they are implemented, so doing so is fine and will not
+ * have an effect on other functions.
+ */
