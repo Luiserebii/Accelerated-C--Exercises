84a93fbdb0393762318cedf122668fc343ff175e
Luiserebii <luis@serebii.io>
2019-11-08 00:32:44 -0500

    Adding test code... let's hope this works

--- chapter_exercises/ch11/11_6/main.cpp
+++ chapter_exercises/ch11/11_6/main.cpp
@@ -1,6 +1,36 @@
 #include "vector.h"
+#include <iostream>
+
+using std::ostream;
+using std::cout;
+using std::endl;
+
+template <class T>
+void print(T begin, T end, ostream& out=cout);
 
 int main() {
 
+    //Setting up a Vec
+    Vec<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(4);
+    v.push_back(8);
+    print(v.begin(), v.end());
+
+    //Attempt erase()
+    v.erase(3);
+    print(v.begin(), v.end());
+    
+
 
 }
+
+template <class T>
+void print(T begin, T end, ostream& out) {
+    while(begin != end) {
+        out << *begin++ << " ";
+    }
+    out << endl;
+}
+
