df504aecfdd4e50192243c2c1652a35dc0de3570
Luiserebii <luis@serebii.io>
2019-11-14 20:10:44 -0500

    Working on 12-2... c_str() could be quite a bit of work considering the memory management

---
+++ chapter_exercises/ch12/12_2/string.cpp
@@ -0,0 +1,43 @@
+#include "string.h"
+#include <iostream>
+
+using std::istream;
+
+istream& operator>>(istream& is, Str& s) {
+
+    //Obliterate existing value(s)
+    s.data.clear();
+
+    //Read and discard leading whitespace
+    char c;
+    while(is.get(c) && isspace(c));
+
+    //If still something to read, do so until next whitespace character
+    if(is) {
+        do {
+            s.data.push_back(c);
+        } while(is.get(c) && !isspace(c));
+    
+        //If we've read whitespace, place it back on the stream - unget
+        if(is) {
+            is.unget();
+        }
+    }
+}
+
+Str operator+(const Str& s, const Str& t) {
+    Str r = s;
+    r += t;
+    return r;
+}
+
+void Str::updateCStr() {
+    if(cstr_raw != 0) {
+        delete[] str_raw
+    }
+    char*
+    Vec<char>::iterator i = data.begin();
+    while(i != data.end()) {
+        
+    }
+}
---
+++ chapter_exercises/ch12/12_2/string.h
@@ -0,0 +1,62 @@
+#ifndef GUARD_String_h
+#define GUARD_String_h
+
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+#include "vector.h"
+
+class Str {
+
+    friend std::istream& operator>>(std::istream& is, Str& s);
+
+    public:
+        typedef Vec<char>::size_type size_type;
+
+        //Default constructor
+        Str() { }
+
+        //Create a Str containing n copies of c char
+        Str(size_type n, char c): data(n, c) { }
+
+        //Create a Str from a null-terminated array of char
+        Str(const char* cp) {
+            std::copy(cp, cp + std::strlen(cp), std::back_inserter(data));
+        }
+
+        //Create a Str from the range denoted by iterators b and e
+        template <class In>
+        Str(In b, In e) {
+            std::copy(b, e, std::back_inserter(data));
+        }
+
+        //Operators
+        char& operator[](size_type i) { return data[i]; }
+        const char& operator[](size_type i) const { return data[i]; }
+
+        Str& operator+=(const Str& s) {
+            std::copy(s.data.begin(), s.data.end(), std::back_inserter(data));
+            return *this;
+        }
+
+        size_type size() const { return data.size(); }
+        char* c_str() { return cstr_raw; }
+
+    private:
+        Vec<char> data;
+        char* cstr_raw;
+
+        void updateCStr();
+};
+
+Str operator+(const Str& s, const Str& t);
+
+inline 
+std::ostream& operator<<(std::ostream& os, const Str& s) {
+    for(Str::size_type i = 0; i < s.size(); ++i) {
+        os << s[i];
+    }
+    return os;
+}
+
+#endif
