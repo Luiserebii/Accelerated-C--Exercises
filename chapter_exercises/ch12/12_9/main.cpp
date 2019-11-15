157f04b6a17accb2507ad970937d9272663c6812
Luiserebii <luis@serebii.io>
2019-11-15 01:13:16 -0500

    Finishing up 12-9 with thoughts

--- chapter_exercises/ch12/12_9/main.cpp
+++ chapter_exercises/ch12/12_9/main.cpp
@@ -12,4 +12,9 @@ int main() {
     cout << "s value: " << s << endl;
     cout << "c_str(): " << s.c_str() << endl;
 
+    /**
+     * I don't quite know why it wasn't asked us to reimplement the input
+     * operator with istream_iterator; does it have to do with being able
+     * to unget()? Couldn't we just call it on the original stream?
+     */
 }  
